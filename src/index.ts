17730799c8c8570a0c5dcdd5e0e59160feb032d1
wangfeijian <wangfeijianhao@163.com>
2026-07-11 08:49:53 +0800

    feat: read log file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello World");
+import * as fs from "node:fs";
+
+console.log("Industrial Log Analyzer");
+console.log("")
+
+const text = fs.readFileSync("data/sample.log", "utf-8");
+
+console.log(text);
